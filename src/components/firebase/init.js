6ef03610e376299826f894943cb994774dabfcdb
Huy Dang Tuan <huy.dang@orientsoftware.com>
2019-05-03 08:58:40 +0700

    Init

---
+++ src/components/firebase/init.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC8ulgbXLsAOq0C73m5PYXuWbkbFJDu6RU",
+    authDomain: "vue-ex-6b69c.firebaseapp.com",
+    databaseURL: "https://vue-ex-6b69c.firebaseio.com",
+    projectId: "vue-ex-6b69c",
+    storageBucket: "vue-ex-6b69c.appspot.com",
+    messagingSenderId: "280041007923"
+  };
+  firebase.initializeApp(config);
